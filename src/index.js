fed842da8fbf90231d52c5bd3fc744945c7e1955
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-09 19:19:08 +0700

    integrate bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const element = <h1>hello world</h1>;
 
