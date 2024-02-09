b625806ed0a109ac391958832065da9f89ee0e63
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-09 19:07:50 +0700

    component rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Navbar from "./components/navbar";
 
 const element = <h1>hello world</h1>;
 
-ReactDom.render(element, document.getElementById("root"));
+ReactDom.render(<Navbar />, document.getElementById("root"));
