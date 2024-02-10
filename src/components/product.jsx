d8ab25d36036c4c2a4b735092727505dae6f9fbb
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-10 11:39:14 +0700

    solve styles bug

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,7 +4,7 @@ class Product extends Component {
   state = {};
   render() {
     return (
-      <div className="card" style="width: 18rem;">
+      <div className="card" styles={{ width: "18rem" }}>
         <img
           src="./assets/img/tomato.jpeg"
           className="card-img-top"
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDom from "react-dom";
 import Navbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Product from "./components/product";
 
 const element = <h1>hello world</h1>;
 
-ReactDom.render(<Navbar />, document.getElementById("root"));
+// ReactDom.render(<Navbar />, document.getElementById("root"));
+ReactDom.render(<Product />, document.getElementById("root"));
