6db075bcde41b1ba50e1a3d80f7a1dce97d32b3b
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-10 11:51:14 +0700

    create app component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Navbar from "./components/navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar";
 import Product from "./components/product";
 
 const element = <h1>hello world</h1>;
 
-// ReactDom.render(<Navbar />, document.getElementById("root"));
-ReactDom.render(<Product />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
