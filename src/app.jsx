35b6d759abe2aa03a63c71bcea7c743deb807777
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-17 13:41:06 +0100

    shopping cart component

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Navbar from "./components/navbar";
 import Product from "./components/product";
+import ShoppingCart from "./components/shopping-cart";
 
 class App extends Component {
   state = {};
@@ -9,23 +10,30 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <div className="product-container">
-          <Product
-            image="tomato.jpeg"
-            title="tomatos"
-            description="red and tasty"
-          />
-          <Product
-            image="cucumber.jpeg"
-            title="cucumber"
-            description="green and long"
-          />
-          <Product image="apples.jpeg" title="apples" description="delicious" />
-          <Product
-            image="mango.jpeg"
-            title="mango"
-            description="sweet and exotic"
-          />
+        <div className="main-container">
+          <div className="product-container">
+            <Product
+              image="tomato.jpeg"
+              title="tomatos"
+              description="red and tasty"
+            />
+            <Product
+              image="cucumber.jpeg"
+              title="cucumber"
+              description="green and long"
+            />
+            <Product
+              image="apples.jpeg"
+              title="apples"
+              description="delicious"
+            />
+            <Product
+              image="mango.jpeg"
+              title="mango"
+              description="sweet and exotic"
+            />
+          </div>
+          <ShoppingCart></ShoppingCart>
         </div>
       </div>
     );
---
+++ src/components/shopping-cart.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ShoppingCart extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h2>Shopping Cart</h2>
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
