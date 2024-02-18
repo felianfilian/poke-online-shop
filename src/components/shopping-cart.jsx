ba295a8349358bef35449fe50f6d4e7a96b66909
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-02-18 08:45:37 +0100

    render states

--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 
 class ShoppingCart extends Component {
-  state = {};
+  state = {
+    items: [
+      {
+        amount: 1,
+        name: "Tomaten",
+        price: 2.9,
+      },
+      {
+        amount: 1,
+        name: "Äpfel",
+        price: 4.9,
+      },
+    ],
+  };
   render() {
     return (
       <div className="shopping-cart">
         <h2>Shopping Cart</h2>
+        {this.state.items.map((item) => (
+          <div>
+            {item.amount}x {item.name} - {item.price}
+          </div>
+        ))}
       </div>
     );
   }
